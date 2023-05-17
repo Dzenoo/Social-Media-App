77cd528dc45979d18e12f5e96c4cacdb39ad8c3d
Dzenoo <frontprogramer@gmail.com>
2023-05-17 14:45:07 +0200

    Adding signup

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { Button, TextField, Typography } from "@mui/material";
+import classes from "../../../css/Auth.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Signup = () => {
+  return (
+    <section className={classes.signup_section}>
+      <div className={classes.empty_div}></div>
+      <div className={classes.signup_form_div}>
+        <Typography color="#333" variant="h4" fontWeight="bold">
+          CREATE ACCOUNT
+        </Typography>
+        <Typography color="textSecondary" variant="p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          aperiam aut saepe ipsum earum hic illum totam sit assumenda. Sequi
+          placeat eos magnam reiciendis hic esse, omnis consequuntur. Dicta,
+          laudantium!
+        </Typography>
+        <form className={classes.signup_form}>
+          <TextField placeholder="Enter username" label="Username" />
+          <TextField placeholder="Enter email" label="Email" />
+          <TextField placeholder="Enter your password" label="Password" />
+          <TextField
+            placeholder="Confirm your password..."
+            label="Confirm password"
+          />
+          <div className={classes.signup_actions}>
+            <div className={classes.google}>
+              <Image
+                src="/images/search.png"
+                alt="google"
+                width={40}
+                height={40}
+              />
+            </div>
+            <Button
+              type="submit"
+              size="large"
+              variant="contained"
+              sx={{ backgroundColor: "#006ccf", width: "100%" }}
+            >
+              Register
+            </Button>
+          </div>
+        </form>
+        <Typography>
+          You already have account? <Link href="/login">Click here</Link>
+        </Typography>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
