e4047997f781438f14c2112d8c94d8863b3e83a3
Dzenoo <frontprogramer@gmail.com>
2023-05-18 16:49:23 +0200

    Adding posts page

--- app/(dashboard)/posts/page.jsx
+++ app/(dashboard)/posts/page.jsx
@@ -1,7 +1,45 @@
 "use client";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import classes from "../../../css/Posts.module.css";
+import Image from "next/image";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  return (
+    <section className={classes.main_dashboard}>
+      <div className={classes.filter_bar}>
+        <Typography variant="h5" fontWeight="bold" sx={{ padding: "20px" }}>
+          Posts
+        </Typography>
+        <FormControl sx={{ width: "200px" }}>
+          <InputLabel>FIlter</InputLabel>
+          <Select placeholder="Filter">
+            <MenuItem>Hashtags</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ width: "200px" }}>
+          <TextField label="Search" />
+        </FormControl>
+        <FormControl>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ display: "flex", alignItems: "center", gap: "12px" }}
+          >
+            Create new post
+            <Image src="/images/add.png" width={30} height={30} />
+          </Button>
+        </FormControl>
+      </div>
+    </section>
+  );
 };
 
 export default Posts;
---
+++ components/Posts/PostItem.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+const PostItem = () => {
+  return <div>PostItem</div>;
+};
+
+export default PostItem;
