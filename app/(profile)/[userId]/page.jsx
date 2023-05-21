95a1c8e5b7a1dac1c8a72af49f6317f976272132
Dzenoo <frontprogramer@gmail.com>
2023-05-21 16:45:02 +0200

    Finishing profile

--- app/(profile)/[userId]/page.jsx
+++ app/(profile)/[userId]/page.jsx
@@ -1,13 +1,24 @@
 "use client";
 
+import Post from "@/components/Home/Post";
 import UserProfileCard from "@/components/Profile/UserProfileCard";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+
+const userPosts = 4;
 
 const UserProfile = () => {
   return (
     <Container maxWidth="lg">
-      {/* PRofile Card */}
       <UserProfileCard />
+      {userPosts === 0 ? (
+        <Typography variant="h4" align="center" marginTop="40px">
+          User have no posts
+        </Typography>
+      ) : (
+        <Container maxWidth="md">
+          <Post />
+        </Container>
+      )}
     </Container>
   );
 };
