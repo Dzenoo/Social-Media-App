a70afabdd34baca12cc4a44d709657f344338303
Dzenoo <frontprogramer@gmail.com>
2023-05-21 16:34:47 +0200

    dding profile view

--- app/(profile)/[userId]/page.jsx
+++ app/(profile)/[userId]/page.jsx
@@ -1,7 +1,15 @@
 "use client";
 
+import UserProfileCard from "@/components/Profile/UserProfileCard";
+import { Container } from "@mui/material";
+
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  return (
+    <Container maxWidth="lg">
+      {/* PRofile Card */}
+      <UserProfileCard />
+    </Container>
+  );
 };
 
 export default UserProfile;
---
+++ components/Profile/UserProfileCard.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import classes from "../../css/Profile.module.css";
+
+const UserProfileCard = () => {
+  return (
+    <Card className={classes.user_profile_card}>
+      <Box className={classes.landingImage}>
+        <Image src="/images/landing.jpg" width={100} height={100} />
+      </Box>
+      <CardContent className={classes.profile_content}>
+        <Image src="/images/exampleimg.jpg" width={120} height={120} />
+        <Box className={classes.profile_view_info}>
+          <Typography variant="h6" fontWeight="bold">
+            John Doe
+          </Typography>
+          <Typography color="textSecondary" variant="p">
+            johndoe@gmail.com
+          </Typography>
+        </Box>
+        <Box className={classes.profile_view_flw}>
+          <Typography variant="p" className={classes.profile_flws}>
+            <strong>1200</strong>
+            followers
+          </Typography>
+          <Typography variant="p" className={classes.profile_flws}>
+            <strong>12</strong>
+            following
+          </Typography>{" "}
+          <Typography variant="p" className={classes.profile_flws}>
+            <strong>2</strong>
+            posts
+          </Typography>
+        </Box>
+        <Button variant="contained">Follow</Button>
+      </CardContent>
+      <CardActions className={classes.profile_view_actions}>
+        <Typography variant="h6" fontWeight="bold">
+          Bio
+        </Typography>
+        <Typography color="textSecondary" variant="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default UserProfileCard;
