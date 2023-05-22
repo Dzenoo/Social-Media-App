bc0cc1c02b5dff952e88fb4826a889331f36a8ca
Dzenoo <frontprogramer@gmail.com>
2023-05-22 23:42:48 +0200

    Creating schema

--- models/user.jsx
+++ models/user.jsx
@@ -1,12 +1,24 @@
-import { Schema, model, models } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: [true, "First name is required!"],
+  },
+  last_name: {
+    type: String,
+    required: [true, "Last name is required!"],
+  },
   email: {
     type: String,
     unique: [true, "Email already exist!"],
     required: [true, "Email is required!"],
   },
-  username: {
+  biography: {
+    type: String,
+    required: [true, "Biography is required!"],
+  },
+  password: {
     type: String,
     required: [true, "Password is required!"],
   },
