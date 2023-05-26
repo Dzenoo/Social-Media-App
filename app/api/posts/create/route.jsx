1e240bd67328aa86eb39ae6fcbe40e6d2d750bdb
Dzenoo <frontprogramer@gmail.com>
2023-05-26 19:16:51 +0200

    Adding api

---
+++ app/api/posts/create/route.jsx
@@ -0,0 +1,32 @@
+import Post from "@/models/post";
+import { connectToDB } from "@/utils/database";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const POST = async (request) => {
+  try {
+    await connectToDB();
+
+    const { location, hashtags, image, description } = await request.json();
+
+    const photoUrl = await cloudinary.uploader.upload(image);
+
+    const newPost = await Post.create({
+      location,
+      hashtags,
+      image: photoUrl.url,
+      description,
+    });
+
+    await newPost.save();
+
+    return new Response("Added", { status: 201 });
+  } catch (error) {
+    return new Response("Could not connect with mongodb", { status: 500 });
+  }
+};
