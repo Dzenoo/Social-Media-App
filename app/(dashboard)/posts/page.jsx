256b694398f5e22d1e60947dec392452061dfe01
Dzenoo <frontprogramer@gmail.com>
2023-05-28 14:49:04 +0200

    Finishing likes func

--- app/(dashboard)/posts/page.jsx
+++ app/(dashboard)/posts/page.jsx
@@ -89,7 +89,7 @@ const Posts = () => {
                 description={post.description}
                 image={post.image}
                 date={post.createdAt}
-                likes={post.likes}
+                likes={post.likes.length}
                 comments={post.comments.length}
                 shares={post.shares.length}
                 hashtags={post.hashtags}
