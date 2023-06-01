87ff363853a6051a0e29c073e02fdb5318bf4e41
Dzenoo <frontprogramer@gmail.com>
2023-06-01 18:34:13 +0200

    Removing functions

--- utils/functions.jsx
+++ utils/functions.jsx
@@ -1,22 +1,5 @@
-export const getPostById = async (id) => {
-  try {
-    const response = await fetch(`/api/posts/${id}`);
-    const post = await response.json();
-    return post;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export const getPosts = async () => {
   const response = await fetch("/api/posts");
-  const responseData = await response.json();
-
-  return responseData;
-};
-
-export const getUser = async (uId) => {
-  const response = await fetch(`/api/users/${uId}`);
   const responseData = await response.json();
 
   return responseData;
