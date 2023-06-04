92f40a14e247413572333d97b6c8277237831c7c
Dzenoo <frontprogramer@gmail.com>
2023-06-04 15:35:04 +0200

    Removing postlis

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,69 @@
 "use client";
 import LandingPage from "@/components/Home/LandingPage";
-import PostList from "@/components/Posts/PostList";
+import useSwr from "swr";
+import { Typography } from "@mui/material";
 import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import Post from "@/models/post";
 
 export default function Home() {
+  const [allPosts, setallPosts] = useState([]);
   const token =
     typeof window !== "undefined"
       ? JSON.parse(localStorage.getItem("userdata"))
       : null;
+  const userInfo =
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("userinfo"))
+      : null;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data } = useSwr(`/api/users/${userInfo.userId}`, fetcher);
+  const { data: postsData } = useSwr("/api/posts", fetcher);
+
+  useEffect(() => {
+    if (postsData) {
+      const recentPosts = postsData.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+      setallPosts(recentPosts);
+    }
+  }, [postsData]);
 
   return (
     <>
       {token?.token ? (
         <Container maxWidth="md" sx={{ padding: "20px" }}>
-          <PostList />
+          {allPosts.length === 0 ? (
+            <Typography textAlign="center" variant="h4">
+              No posts jet
+            </Typography>
+          ) : (
+            allPosts
+              .filter(
+                (post) =>
+                  post.creator.isPrivate === false ||
+                  data?.following.includes(post.creator._id) ||
+                  post.creator._id === userInfo.userId
+              )
+              .map((post) => (
+                <Post
+                  key={post._id}
+                  postId={post._id}
+                  hashtags={post.hashtags}
+                  description={post.description}
+                  image={post.image}
+                  date={post.createdAt}
+                  location={post.location}
+                  firstName={post.creator.first_name}
+                  lastName={post.creator.last_name}
+                  creatorImg={post.creator.image}
+                  userId={post.creator._id}
+                  likes={post.likes}
+                  comments={post.comments}
+                  show={true}
+                />
+              ))
+          )}
         </Container>
       ) : (
         <LandingPage />
