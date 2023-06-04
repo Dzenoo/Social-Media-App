a6c62072c264e837b4e69f3ca896dcfd1a5ab5b4
Dzenoo <frontprogramer@gmail.com>
2023-06-04 15:05:56 +0200

    New fix

--- components/Posts/PostList.jsx
+++ components/Posts/PostList.jsx
@@ -28,24 +28,31 @@ const PostList = () => {
           No posts jet
         </Typography>
       ) : (
-        allPosts.map((post) => (
-          <Post
-            key={post._id}
-            postId={post._id}
-            hashtags={post.hashtags}
-            description={post.description}
-            image={post.image}
-            date={post.createdAt}
-            location={post.location}
-            firstName={post.creator.first_name}
-            lastName={post.creator.last_name}
-            creatorImg={post.creator.image}
-            userId={post.creator._id}
-            likes={post.likes}
-            comments={post.comments}
-            show={true}
-          />
-        ))
+        allPosts
+          .filter(
+            (post) =>
+              post.creator.isPrivate === false ||
+              data?.following.includes(post.creator._id) ||
+              post.creator._id === userInfo.userId
+          )
+          .map((post) => (
+            <Post
+              key={post._id}
+              postId={post._id}
+              hashtags={post.hashtags}
+              description={post.description}
+              image={post.image}
+              date={post.createdAt}
+              location={post.location}
+              firstName={post.creator.first_name}
+              lastName={post.creator.last_name}
+              creatorImg={post.creator.image}
+              userId={post.creator._id}
+              likes={post.likes}
+              comments={post.comments}
+              show={true}
+            />
+          ))
       )}
     </div>
   );
